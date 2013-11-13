af0eb6f1dae47ae0af8bd6f15deb40ce17077920
Rahim Ladak <rahim.ladak@gmail.com>
2013-11-12 19:02:50 -0600

    Completed Part 1 of HW_Week 7

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,41 @@
 class PicturesController < ApplicationController
   def show
+  	@picture = Picture.find(params[:id])
+  end
+
+  def index
+  	@list_of_pictures = Picture.all
+  end
+
+  def new
+end
+
+def create
+  p = Picture.new
+  p.source = params[:source]
+  p.caption = params[:caption]
+  p.save
+  
+  redirect_to pictures_url, :notice => "You have created a new picture" 
+end
+
+def update
+  p = Picture.find(params[:id])
+  p.source = params[:source]
+  p.caption = params[:caption]
+  p.save
+  redirect_to picture_url(p.id), :notice => "You have updated the picture"
+end
+
+def edit
+  @picture = Picture.find(params[:id])
+
+end
+
+  def destroy
+  	p = Picture.find(params[:id])
+  	p.destroy
+  	redirect_to pictures_url, :notice => "You have deleted the picture"
+ 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rxngif::Application.routes.draw do
-  get("/picture_details/:id", { :controller => "pictures", :action => "show" })
+   
+  # CREATE
+  get "/pictures/new", :controller => "pictures", :action => "new", :as => "new_picture"
+  post "/pictures", :controller => "pictures", :action => "create", :as => "pictures"
+
+  # READ
+  get "/pictures", :controller => "pictures", :action => "index"
+  get "/pictures/:id", :controller => "pictures", :action => "show", :as => "picture"
+
+  #UPDATE
+  get "/pictures/:id/edit", :controller => "pictures", :action => "edit", :as => "edit_picture"
+  patch "/pictures/:id", :controller => "pictures", :action => "update"
+
+  # DELETE
+  delete "/pictures/:id", :controller => "pictures", :action => "destroy"
 end
